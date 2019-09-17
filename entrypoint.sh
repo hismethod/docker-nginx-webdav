4e290535b3713c102e07b72e9fc9c54b01f27aca
hismethod <his.methods@gmail.com>
2019-09-17 17:36:45 +0900

    commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 htpasswd -bc /etc/nginx/.webdav_root $USERNAME $PASSWORD
-htpasswd -bc /etc/nginx/.webdav_user $USERNAME1 $PASSWORD1
+cp /etc/nginx/.webdav_root /etc/nginx/.webdav_user
+htpasswd -b /etc/nginx/.webdav_user $USERNAME1 $PASSWORD1
